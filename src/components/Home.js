94cff9bacd61905486b8f6baa56e4ed00492726b
Chris Beard <cbeard@hnoj.org>
2021-09-14 21:48:41 -0500

    debug image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import { createRequire } from 'module';
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import picture from '/chrisbeard.jpg';
+import picture from './chrisbeard.jpg';
 console.log(picture);
 class Home extends Component {
     render() {
