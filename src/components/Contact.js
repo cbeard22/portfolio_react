01a670a259468676f3c4b305fdcdf06bd35b9c02
Chris Beard <cbeard@hnoj.org>
2021-09-16 19:17:27 -0500

    debugging images

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
-import img from '../img/chrisBeard2.jpg';
+import img from '../img/chrisbeard2.jpg';
 
 class Contact extends Component {
     render() {
